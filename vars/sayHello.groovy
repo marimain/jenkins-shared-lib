921b4f56f17b5774d6463aa74811dd07faf79d03
hehaibo@hotmail.com <hehaibo@hotmail.com>
2024-01-23 12:45:16 -0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,14 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-   pipeline {
-    agent any
-    stages {
-        stage('build') {
-            steps {
-                bat 'echo ruby --version'
-            }
-        }
-    }
-}
+  bat "Hello, ${name}."
 }
