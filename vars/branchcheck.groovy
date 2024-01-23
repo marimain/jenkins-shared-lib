a68041eb354c5911f0bf2dc42e7902786211ef6f
hehaibo@hotmail.com <hehaibo@hotmail.com>
2024-01-23 15:48:58 -0500

    Update branchcheck.groovy

--- vars/branchcheck.groovy
+++ vars/branchcheck.groovy
@@ -1,5 +1,6 @@
 def info(message) {
     echo "INFO: ${message}"
+    mail to: 'hehaibo@hotmail.com', subject: "branchcheck plugin build", body: 'This is a mail from branch check library for info'
 }
 
 def warning(message) {
